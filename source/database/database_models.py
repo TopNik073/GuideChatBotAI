363e6fe0278ca6500da049c94ea7f9bbe91a6f72
TopNik073 <borzovn169@gmail.com>
2024-10-30 21:59:17 +0200

    Remove old pettern

--- source/database/database_models.py
+++
@@ -1,9 +0,0 @@
-from source.database.models.user import Users as UsersModel
-
-
-class Users(UsersModel):
-    pass
-
-
-class Attractions:
-    pass
--- source/handlers/__init__.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router
-
-from .user_handlers import router as user_router
-
-router = Router(name="main_router")
-
-router.include_routers(user_router)
