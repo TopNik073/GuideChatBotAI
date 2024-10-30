9f84e06e1d9e8675102938008c5bd814111c6afd
TopNik073 <borzovn169@gmail.com>
2024-10-30 14:40:46 +0200

    Add Attractions

--- source/database/database_models.py
+++ source/database/database_models.py
@@ -1,9 +1,10 @@
 from source.database.models.user import Users as UsersModel
+from source.database.models.attractions import Attractions as AttractionsModel
 
 
 class Users(UsersModel):
     pass
 
 
-class Attractions:
+class Attractions(AttractionsModel):
     pass
